91be29cc6fe7b12cf95e14cde10f5fcfbdb4dcce
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-19 15:32:38 -0600

    Game libarary linked correctly to main

--- app/main.cpp
+++ app/main.cpp
@@ -2,15 +2,13 @@
 #include <config.hpp>
 
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
-
-// #include <Game.h>
+#include <Game.h>
 
 int main(int argc, char** argv){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
 
-    // Game Life;
+    Game Life;
 
     return 0;
 }

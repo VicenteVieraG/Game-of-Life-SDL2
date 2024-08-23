a0b43a0ec84da600f0a9d0c9884f78c7a385222f
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-08-23 16:45:50 -0600

    Removed wheel scroll implementation

--- src/events/Events.cpp
+++ src/events/Events.cpp
@@ -27,27 +27,5 @@ namespace Handle{
         const auto [scaleDown, scaleUp] = scale;
         const auto [mouseX, mouseY] = Coord{wheel.mouseX, wheel.mouseY};
         const auto [wheelX, wheelY] = Coord{wheel.x, wheel.y};
-        const int displace = -15;
-
-        /* ~~Zoom computation~~ */
-        const float gridX = (mouseX - offsetX) / zoom;
-        const float gridY = (mouseY - offsetY) / zoom;
-
-        if(wheelY > 0) zoom *= scaleUp;
-        if(wheelY < 0) zoom *= scaleDown;
-
-        // New offset
-        offsetX = mouseX - gridX * zoom;
-        offsetY = mouseY - gridY * zoom;
-
-        // Scale cells' size
-        cellW = 1.0f * zoom;
-        cellH = 1.0f * zoom;
-
-        // Horizontal displace
-        if(wheelX){
-            offsetX += static_cast<float>(wheelX * displace);
-            offsetX = std::clamp(offsetX, -50.0f, 50.0f);
-        }
     }
 };
